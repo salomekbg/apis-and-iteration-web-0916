4538b5ab88f05cb1fc481df23995f60049312b73
calebcox725 <caleb.cox@flatironschool.com>
2016-09-28 15:51:56 -0400

    started methods for cli selections

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,9 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+
+# character = get_character_from_user
+# show_character_movies(character)
+
+selection = get_selection_from_user
+
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,3 +7,15 @@ def get_character_from_user
   answer = gets.chomp.downcase
 end
 
+def get_selection_from_user
+  selection = nil
+  while selection == nil
+    puts "Please enter your selection:"
+    puts "  1 - Character"
+    puts "  2 - Film"
+
+    initial_answer = gets.chomp
+
+    selection = initial_answer if initial_answer == "1" || initial_answer == "2"
+  end
+end
